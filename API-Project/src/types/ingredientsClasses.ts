3e2a04c1efb08648286cc63c5d46974da674845b
Remi R <remi.regnault@etu.uca.fr>
2023-12-11 13:43:04 +0100

    feat: add enum for IngredientsClasses

---
+++ API-Project/src/types/ingredientsClasses.ts
@@ -0,0 +1,8 @@
+enum IngredientsClasses {
+    DairyFree = 'DAIRY_FREE', 
+    GlutenFree = 'GLUTEN_FREE', 
+    Porcless = 'PORCLESS',
+    Vegan = 'VEGAN', 
+    Vegetarian = 'VEGETARIAN', 
+    Pescatarian = 'PESCATARIAN'
+}
